0c69544188d6d080a2e02073a4fbb9a7e708ca4b
Pedro <pedroornelaspego@gmail.com>
2023-01-19 23:11:38 -0300

    time dinamico

--- src/App.js
+++ src/App.js
@@ -5,11 +5,49 @@ import Formulario from "./components/Formulario";
 import Time from "./components/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#57c278",
+      corSecundaria: "#d9f7e9",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#F0F8E2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
+    },
+    {
+      nome: "UX e Design",
+      corPrimaria: "#DB6EBF",
+      corSecundaria: "#FAE9F5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "#FFBA05",
+      corSecundaria: "#FFF5D9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8A29",
+      corSecundaria: "#FFEEDF",
+    },
+  ];
+
   const [calaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
     console.log(colaborador);
-    setColaboradores({...calaboradores, colaborador})
+    setColaboradores({ ...calaboradores, colaborador });
   };
 
   return (
@@ -20,9 +58,9 @@ function App() {
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
-      <Time nome="Programação"/>
-      <Time nome="Frontend"/>
-      <Time nome="Data Science"/>
+
+      {times.map(time => <Time nome={time.nome}/>)}
+      
     </div>
   );
 }
